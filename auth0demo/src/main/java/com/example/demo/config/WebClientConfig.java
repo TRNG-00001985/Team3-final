df8f54c003e621d15185210ca809c915bbbeefc3
majaaz <majaaz0014@gmail.com>
2024-09-30 17:58:48 +0530

    changes

--- auth0demo/src/main/java/com/example/demo/config/WebClientConfig.java
+++ auth0demo/src/main/java/com/example/demo/config/WebClientConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.reactive.function.client.WebClient;
- 
+import org.springframework.beans.factory.annotation.Autowired;
 @Configuration
 
 public class WebClientConfig {
--- cart-service/src/main/java/com/revature/revshop/config/WebClientConfig.java
+++ cart-service/src/main/java/com/revature/revshop/config/WebClientConfig.java
@@ -1,6 +1,6 @@
 package com.revature.revshop.config;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
