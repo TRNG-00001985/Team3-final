47ab5bdca3c740d184fdc5fab9a5dca668e4453d
krishnagopika <103101208+krishnagopika@users.noreply.github.com>
2024-09-30 18:26:21 +0530

    Update NotificationServiceApplication.java

--- notification-service/src/main/java/com/revature/revshop/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/revature/revshop/NotificationServiceApplication.java
@@ -2,12 +2,26 @@ package com.revature.revshop;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import com.revature.revshop.event.OrderEvent;
+import com.revature.revshop.config.OrderEventDeserializer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 @SpringBootApplication
 public class NotificationServiceApplication {
